4c7f4f0cffa96dd7b64859193e9bf695de8fab2b
john <johnthomas222000@gmail.com>
2021-01-27 17:16:17 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Magic 8 Ball'),
+          backgroundColor: Colors.red,
+        ),
+        body: Magic8Ball(),
+      ),
+    ),
+  );
+}
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballChange = 1;
+  void changeImage() {
+    setState(() {
+      ballChange = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeImage();
+              },
+              child: Image(
+                image: AssetImage('images/ball$ballChange.png'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
